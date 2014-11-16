a4163eeff3013ef884d64b55c8b761696c4f160a
ian <ian.kerr.redmond@gmail.com>
2014-11-16 23:08:59 +0000

    first working tracking in x axis.

--- facedetectvideo.py
+++ facedetectvideo.py
@@ -1,11 +1,17 @@
+#!/usr/bin/python
+
 import cv
 import sys
+import serial
+import time
 
 CAMERA_VIEW  = 60     # degrees
 CAMERA_WIDTH = 213    # pixels
 
 # Get user supplied values
 cascPath = sys.argv[1]
+portName = sys.argv[2]
+
 # Create the haar cascade
 faceCascade = cv.Load(cascPath)
 
@@ -26,8 +32,26 @@ except:
     sys.exit()
 print "result image size :", image.width/3, image.height/3
 
+try:
+    port = serial.Serial(portName, 9600, timeout=1)
+except:
+    port = None
+
+if port:
+    time.sleep(9)
+    text ='Arduino booting..'
+    while(text!=''):    # print messages until serial timeout
+        print text.rstrip()
+        text = port.readline()
+    print 'Done booting.'
+    
+    port.flush()
+    port.write('?\n')
+    text = port.readline()
+    print text
+
 image_result = cv.CreateImage((image.width/3, image.height/3),8,3)
-while cv.WaitKey(100)==-1:
+while cv.WaitKey(10)==-1:
     image = cv.QueryFrame(capture1)
     cv.Resize(image,image_result)
     # Detect faces in the image
@@ -42,6 +66,13 @@ while cv.WaitKey(100)==-1:
         (x, y, w, h),n = faces[0]
         target_x = x+w/2
         target_y = y+h/3
-        print xToDegrees(target_x), target_y
+        target_x_degrees = xToDegrees(target_x)
+        print target_x_degrees, target_y
+        command = """%s 1s\n"""%(target_x_degrees)
+        print command
+        if port: port.write(command)
 
     cv.ShowImage("Faces found", image_result)
+
+if port:
+    port.close()
